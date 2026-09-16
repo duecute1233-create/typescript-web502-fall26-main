27298f5f6c83e5a520d66e9d05783e1ebc0e4d39
duecute1233-create <duecute1233@gmail.com>
2026-09-16 19:25:14 +0700

    Add files via upload

---
+++ learn-typescript/src/lab2.ts
@@ -0,0 +1,14 @@
+ enum rate {low = 'Thấp', medium = 'Trung bình', hight = 'Cao'}
+const products:{name: string,
+price: number,
+sale: boolean,
+rate: rate
+} =
+{
+    name: "Sample Product",
+    price: 100,
+    sale: true,
+    rate: rate.medium
+
+}
+
---
+++ learn-typescript/src/typescript.ts
@@ -0,0 +1,25 @@
+console.log("typescript");
+ let age: number =30;
+ age = 25;
+
+ let myName: string = "John"; 
+myName = "Doe";
+
+let diHoc: boolean = true;
+diHoc = false;
+
+let myYear = 2026; //number
+myYear = 20234;
+
+
+const student:{ name: string,
+    age: number,
+    diHoc:boolean,
+    myYear: number
+} ={
+    name: "ducnd",
+    age: 25,
+    diHoc: true,
+    myYear: 2026
+
+}
